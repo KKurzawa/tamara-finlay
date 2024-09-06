0a0b93f991a5c3b0887e105c5388f120bea4e658
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-06 16:19:40 -0400

    bug fix

--- src/Components/MobileNavbar/MobileNavbar.jsx
+++ src/Components/MobileNavbar/MobileNavbar.jsx
@@ -50,21 +50,21 @@ const MobileNavbar = () => {
                     id='dropdown'>
                     <ul className='mobile-nav flex flex-col gap-3 w-full py-5 text-xl'>
                         <motion.li
-                            initial={{ left: '100%', opacity: 0 }}
-                            animate={{ left: '6%', opacity: 1 }}
-                            exit={{ left: '100%', opacity: 0 }}
+                            initial={{ left: '100%' }}
+                            animate={{ left: '6%' }}
+                            exit={{ left: '100%' }}
                             transition={{ duration: .2, delay: .5 }}
                             className='mobile-nav-link w-[88%] py-1 pl-3 rounded-xl relative'><button className='w-fit' onClick={navigateToBiography}>Biography</button></motion.li>
                         <motion.li
-                            initial={{ left: '100%', opacity: 0 }}
-                            animate={{ left: '6%', opacity: 1 }}
-                            exit={{ left: '100%', opacity: 0 }}
+                            initial={{ left: '100%' }}
+                            animate={{ left: '6%' }}
+                            exit={{ left: '100%' }}
                             transition={{ duration: .2, delay: .7 }}
                             className='mobile-nav-link w-[88%] py-1 pl-3 rounded-xl relative'><button className='w-fit' onClick={navigateToAnimations}>Animations</button></motion.li>
                         <motion.li
-                            initial={{ left: '100%', opacity: 0 }}
-                            animate={{ left: '6%', opacity: 1 }}
-                            exit={{ left: '100%', opacity: 0 }}
+                            initial={{ left: '100%' }}
+                            animate={{ left: '6%' }}
+                            exit={{ left: '100%' }}
                             transition={{ duration: .2, delay: .9 }}
                             className='mobile-nav-link w-[88%] py-1 pl-3 rounded-xl relative'><button className='w-fit' onClick={navigateToContact}>Contact</button></motion.li>
                     </ul>
