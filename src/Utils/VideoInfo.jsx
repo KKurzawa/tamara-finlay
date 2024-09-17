f32e034d6353c9115f65feea633ed7a113fbf8a7
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-17 15:03:19 -0400

    added video links

--- src/Utils/VideoInfo.jsx
+++ src/Utils/VideoInfo.jsx
@@ -1,28 +1,23 @@
 const videoInfo = [
     {
         key: 1,
-        url: "https://player.vimeo.com/video/102435491?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        title: 'Stop Motion Animation 1'
+        url: "https://player.vimeo.com/video/1010149126?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        title: 'Missing or Incomplete'
     },
     {
         key: 2,
-        url: "https://player.vimeo.com/video/6132161?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        title: 'Stop Motion Animation 2'
+        url: "https://player.vimeo.com/video/1010321737?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        title: 'Baby Yaga'
     },
     {
         key: 3,
-        url: "https://player.vimeo.com/video/65606724?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        title: 'Stop Motion Animation 3'
+        url: "https://player.vimeo.com/video/1010151305?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        title: 'Cover Your Eyes'
     },
     {
         key: 4,
         url: "https://player.vimeo.com/video/199334148?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
         title: 'Stop Motion Animation 4'
-    },
-    {
-        key: 5,
-        url: "https://player.vimeo.com/video/84977684?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        title: 'Stop Motion Animation 5'
     },
 ]
 
