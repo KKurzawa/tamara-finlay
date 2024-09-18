8c1b5ac778b831997cebddd2973507384572e665
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-18 16:49:52 -0400

    bug fix in video

--- src/Utils/VideoInfo.jsx
+++ src/Utils/VideoInfo.jsx
@@ -1,22 +1,22 @@
 const videoInfo = [
     {
         key: 0,
-        url: "https://player.vimeo.com/video/1010149126?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        url: "https://player.vimeo.com/video/1010149126?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?controls=1",
         title: 'Missing or Incomplete'
     },
     {
         key: 1,
-        url: "https://player.vimeo.com/video/1010321737?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        url: "https://player.vimeo.com/video/1010321737?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?controls=1",
         title: 'Baby Yaga'
     },
     {
         key: 2,
-        url: "https://player.vimeo.com/video/1010151305?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        url: "https://player.vimeo.com/video/1010151305?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?controls=1",
         title: 'Cover Your Eyes'
     },
     {
         key: 3,
-        url: "https://player.vimeo.com/video/383934719?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        url: "https://player.vimeo.com/video/383934719?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?controls=1",
         title: 'Uncle Andrei'
     },
 ]
